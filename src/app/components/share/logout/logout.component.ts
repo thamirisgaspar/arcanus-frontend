d58ee520cd31695eac9399b69e7f70fccb7b90ef
Thamiris Cristina <thamiris_cristina_962@hotmail.com>
2023-02-22 19:10:06 -0300

    share module

--- src/app/components/share/logout/logout.component.ts
+++ src/app/components/share/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class LogoutComponent {
 
+  constructor(private router: Router) { }
+
+  logout(){
+    localStorage.removeItem('userid');
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    localStorage.removeItem('name');
+    localStorage.removeItem('id');
+
+    this.router.navigate(['/users'], {queryParams: {'login' : 'active true'}});
+  }
 }
