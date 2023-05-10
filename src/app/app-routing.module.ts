658b71fe830e36e9a933560e55029d6ee2f59a49
Thamiris Cristina <thamiris_cristina_962@hotmail.com>
2023-05-10 13:39:05 -0300

    guard route canactive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CanActiveGuard } from './shared/guards/can-active.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }, 
   { path: 'forgotPassword', loadChildren: () => import('./users/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) }, 
   { path: 'resetPassword', loadChildren: () => import('./users/reset-password/reset-password.module').then(m => m.ResetPasswordModule) },
-  { path: 'arcanus', loadChildren: () => import('./arcanus/arcanus.module').then(m => m.ArcanusModule) },
+  { path: 'arcanus', loadChildren: () => import('./arcanus/arcanus.module').then(m => m.ArcanusModule), canActivate: [CanActiveGuard] },
   { path: '**', redirectTo: '/users'}
 ];
 
 @NgModule({
   //imports: [RouterModule.forRoot(routes, {useHash: true})],
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/shared/guards/can-active.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CanActiveGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (localStorage.getItem('token') != '' && localStorage.getItem('token') != undefined) {
+      return true;
+    }
+
+    this.router.navigate(['/users'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
+  
+}
